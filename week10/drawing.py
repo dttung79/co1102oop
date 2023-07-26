e8be806045975ad06cd12f43382a0dc0b4462e6c
dttung79 <dttung@gmail.com>
2023-07-26 14:07:52 +0700

    add circle exercise

--- week10/drawing.py
+++ week10/drawing.py
@@ -14,9 +14,9 @@ class Game:
         self.CYAN = (0, 255, 255)
         self.YELLOW = (255, 255, 0)
         
-    def draw_rectangle(self, position, size):
+    def draw_rectangle(self, x, y, w, h):
         # Draw a red rectangle
-        pygame.draw.rect(self.window, self.RED, (*position, *size))
+        pygame.draw.rect(self.window, self.RED, (x, y, w, h))
         
     def draw_circle(self, center, radius):
         # Draw a green circle
@@ -26,11 +26,19 @@ class Game:
         # Draw a blue line
         pygame.draw.line(self.window, self.BLUE, start_position, end_position, width)
 
+    def draw_triangle(self, point1, point2, point3):
+        pygame.draw.polygon(self.window, self.CYAN, (point1, point2, point3))
+
+    def draw_ellipse(self, x, y, w, h):
+        pygame.draw.ellipse(self.window, self.YELLOW, (x, y, w, h))
+
 
     def draw_shapes(self):
-        self.draw_rectangle((50, 50), (200, 100))
+        self.draw_rectangle(50, 50, 200, 100)
         self.draw_circle((400, 300), 50)
         self.draw_line((600, 100), (700, 500), 5)
+        self.draw_triangle((100, 500), (200, 500), (150, 400))
+        self.draw_ellipse(200, 50, 200, 100)
 
     def run(self):
         while True:
